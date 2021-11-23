7003c99af8069f1857f5fc4d1c887cbd783fb3d5
higorviegas <viegas.higor@gmail.com>
2021-11-23 14:10:34 -0300

    Service layer, component registration

---
+++ src/main/java/com/higorviegas/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.higorviegas.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.higorviegas.course.entities.User;
+import com.higorviegas.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
